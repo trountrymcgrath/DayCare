1668058c4ac4d40385fc9bc90c2f99b7a3ae9810
trountrymcgrath <trountrymcgrath@student.bridgew.edu>
2020-02-27 01:01:54 -0500

    1. initial class constructing 2. further class constructing

--- src/daycare/comp152/DayCareFacility.java
+++ src/daycare/comp152/DayCareFacility.java
@@ -1,12 +1,31 @@
 package daycare.comp152;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class DayCareFacility {
     private String students = ArrayList<Child>;
     private String employees = ArrayList<DayCareWorker>;
 
-    public RunDayCare();
 
-    public static void main(String[] args)
+
+    public runDayCare(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+    private void printMenu(){
+        System.out.println("Welcome Day Care Administrator.\n"
+        "Please select an option from the menu options:");
+        System.out.println("[1] Admit Child");
+        System.out.println("[2] Hire Worker");
+        System.out.println("[3] Remove Students 6 or Older");
+        System.out.println("[4] Run Fire Drill");
+        System.out.println("[5] Exit");
+        System.out.println("[6] End Of The Year");
+        System.out.println("[7] Do Accounting");
+    }
 }
